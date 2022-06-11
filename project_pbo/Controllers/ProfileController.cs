236358a9f0546dbff7d9874b10afeddcfae20a38
Robin Hood <yoga3911@gmail.com>
2022-06-11 20:26:23 +0700

    update profile

---
+++ project_pbo/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using project_pbo.Models;
+using System.Diagnostics;
+
+namespace project_pbo.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ILogger<ProfileController> _logger;
+
+        public ProfileController(ILogger<ProfileController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
