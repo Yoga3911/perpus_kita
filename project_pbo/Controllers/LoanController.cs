aa4df1bb528927500a4d115a2a194b4be2c9355e
112358Sean <seanthony99@gmail.com>
2022-06-25 14:22:35 +0700

    add loancontroller

---
+++ project_pbo/Controllers/LoanController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_pbo.Models;
+using System.Diagnostics;
+using project_pbo.Services;
+
+namespace project_pbo.Controllers
+{
+    public class LoanController : Controller
+    {
+        private readonly ILogger<LoanController> _logger;
+        LoanService loanService = new LoanService();
+
+        public LoanController(ILogger<LoanController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
